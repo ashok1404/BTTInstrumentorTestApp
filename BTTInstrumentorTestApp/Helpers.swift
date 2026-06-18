a32e40077245f90542b3032a2bd150575a3c8f9f
jp-aloha <jaib@alohatechnology.com>
2026-06-18 18:32:27 +0530

    Initial commit

--- BTTInstrumentorTestApp/Helpers.swift
+++ BTTInstrumentorTestApp/Helpers.swift
@@ -2,13 +2,82 @@ import SwiftUI
 
 enum Tab { case home, profile, extra }
 
-struct HeaderView: View { var body: some View { EmptyView() } }
-struct FooterView: View { var body: some View { EmptyView() } }
-struct MainView: View { var body: some View { EmptyView() } }
-struct DetailView: View { var body: some View { EmptyView() } }
-struct LoginView: View { var body: some View { EmptyView() } }
-struct HomeView: View { var body: some View { EmptyView() } }
-struct ProfileView: View { var body: some View { EmptyView() } }
-struct DebugView: View { var body: some View { EmptyView() } }
-struct ReleaseView: View { var body: some View { EmptyView() } }
-struct LoadingView: View { var body: some View { EmptyView() } }
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            Text("Header View")
+        }
+    }
+}
+
+struct FooterView: View {
+    var body: some View {
+        VStack {
+            Text("Footer View")
+        }
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Main View")
+        }
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            Text("Detail View")
+        }
+    }
+}
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("Login View")
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Home View")
+        }
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            Text("Profile View")
+        }
+    }
+}
+
+struct DebugView: View {
+    var body: some View {
+        VStack {
+            Text("Debug View")
+        }
+    }
+}
+
+struct ReleaseView: View {
+    var body: some View {
+        VStack {
+            Text("Release View")
+        }
+    }
+}
+
+struct LoadingView: View {
+    var body: some View {
+        VStack {
+            Text("Loading View")
+        }
+    }
+}
