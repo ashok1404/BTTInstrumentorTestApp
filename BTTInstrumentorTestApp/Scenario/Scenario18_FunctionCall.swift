b19f181a16f86d839d34ea56337c96ae8f6c6b29
jp-aloha <jaib@alohatechnology.com>
2026-06-19 13:52:10 +0530

    fix: Added new fun call scenario

---
+++ BTTInstrumentorTestApp/Scenario/Scenario18_FunctionCall.swift
@@ -0,0 +1,37 @@
+//
+//  Scenario18_FunctionCall.swift
+//  BTTInstrumentorTestApp
+//
+//  Created by Ashok Singh on 19/06/26.
+//
+
+import SwiftUI
+
+struct FunctionCall1: View {
+    var body: some View {
+        myView()
+    }
+    
+    func myView() -> some View {
+        VStack {
+            Text("A")
+        }
+    }
+}
+
+struct ViewContainerView2: View {
+    @State private var flag = false
+    var body: some View {
+        if flag {
+            myView()
+        } else {
+            myView()
+        }
+    }
+    
+    func myView() -> some View {
+        VStack {
+            Text("A")
+        }
+    }
+}
